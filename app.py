5c459ea475fbb40e54531cd7494dcc499ccbff49
Josh Eagle <joshua_eagle@hotmail.co.uk>
2020-12-02 15:20:40 +0000

    changed column names

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if uploaded_file is not None:
     # if user doesn't think this is their plant:
     cnn_model = load_model('/home/jupyter/saved_models/augmented_basic_cnn')
     cnn_preds = cnn_model.predict(X)
-    cnn_preds_df = pd.DataFrame(cnn_preds, columns=np.sort(test_df.genus.unique()))
+    cnn_preds_df = pd.DataFrame(cnn_preds, columns=rename_columns)
     cnn_top_3 =pd.DataFrame(cnn_preds_df.apply(lambda x:list(cnn_preds_df.columns[np.array(x).argsort()[::-1][:3]]), axis=1).to_list(),  columns=['Top1', 'Top2', 'Top3'])
     if cnn_top_3['Top1'] != plant_name:
         pred2 = cnn_top_3['Top1']
