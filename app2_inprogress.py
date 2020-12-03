145cc54fd9c23e652723e004e8d0dfd861f79cad
Anna Kondratiuk <anna.kondratiuk89@gmail.com>
2020-12-03 11:30:46 +0000

    containers

--- app2_inprogress.py
+++ app2_inprogress.py
@@ -62,7 +62,17 @@ if uploaded_file is not None:
     plant.resize((224,224)) # the picturs are not equal
     plant2.resize((224,224))
 
-    st.image([plant,plant2], width=100)
+    #st.image([plant,plant2], width=100)
+
+    col1, col2 = st.beta_columns(2)
+
+    col1.header("Original")
+    col1.image(plant, width=100)
+
+    #grayscale = plant.convert('LA')
+    col2.header("Grayscale")
+    col2.image(plant2, width=144)
+
 #----------------------------buttons -----------------
     plant_features= ['Genus','Details', 'Cultivation', 'Propagation', 'Suggested planting locations and garden types', 'Pruning', 'Pests', 'Diseases ']
     #plant_name = plants_care['Genus name'].iloc[0] # only for internal testing
@@ -211,35 +221,6 @@ if uploaded_file is not None:
     )
 
 #---------------------------------------------------
-
-#code used:
-# bootstrap 4 collapse example
-# components.html(
-#     """
-#     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-#     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-#     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-#     <div id="accordion">
-
-#       <div class="card">
-#         <div class="card-header" id="headingOne">
-#           <h5 class="mb-0">
-#             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-#             Cultivation
-#             </button>
-#           </h5>
-#         </div>
-#         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-#           <div class="card-body">
-#            cultivation
-#           </div>
-#         </div>
-#       </div>
-
-#     </div>
-#     """,
-#     height=600,
-# )
 
 st.markdown("<h1 style='text-align: left; color: green;'>London 5 day weather forecast</h1>", unsafe_allow_html=True)
 
